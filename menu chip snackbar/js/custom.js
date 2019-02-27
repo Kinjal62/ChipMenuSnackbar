648fa39f311fa0c793dd7f6b9c59185ec0dd6a4a
Mitul <panditmitul03@gmail.com>
2019-02-27 10:32:23 +0530

    snackbar onclick event using jquery

--- menu chip snackbar/js/custom.js
+++ menu chip snackbar/js/custom.js
@@ -1,3 +1,14 @@
 mdc.menu.MDCMenu.attachTo(document.querySelector('.mdc-menu'));
 // menu.open = true;
-mdc.snackbar.MDCSnackbar.attachTo(document.querySelector('.mdc-snackbar'));
+const snackbar1 = new mdc.snackbar.MDCSnackbar.attachTo(document.querySelector('.mdc-snackbar1'));
+const snackbar2 = new mdc.snackbar.MDCSnackbar.attachTo(document.querySelector('.mdc-snackbar2'));
+const snackbar3 = new mdc.snackbar.MDCSnackbar.attachTo(document.querySelector('.mdc-snackbar3'));
+$('#baseline').on('click',function(event){
+	snackbar1.open();
+});
+$('#leading').on('click',function(event){
+	snackbar2.open();
+});
+$('#stacked').on('click',function(event){
+	snackbar3.open();
+});
