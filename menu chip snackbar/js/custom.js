72fe9c1371df64391a99c8c3a2e0a27d8a58882b
Mitul <panditmitul03@gmail.com>
2019-02-27 11:34:04 +0530

    menu open using jquery

--- menu chip snackbar/js/custom.js
+++ menu chip snackbar/js/custom.js
@@ -1,14 +1,19 @@
-mdc.menu.MDCMenu.attachTo(document.querySelector('.mdc-menu'));
-// menu.open = true;
+const menu = new mdc.menu.MDCMenu.attachTo(document.querySelector('.mdc-menu'));
+$('#menu1').on('click',function(event){
+	menu.open = true
+});
+
 const snackbar1 = new mdc.snackbar.MDCSnackbar.attachTo(document.querySelector('.mdc-snackbar1'));
-const snackbar2 = new mdc.snackbar.MDCSnackbar.attachTo(document.querySelector('.mdc-snackbar2'));
-const snackbar3 = new mdc.snackbar.MDCSnackbar.attachTo(document.querySelector('.mdc-snackbar3'));
 $('#baseline').on('click',function(event){
 	snackbar1.open();
 });
+
+const snackbar2 = new mdc.snackbar.MDCSnackbar.attachTo(document.querySelector('.mdc-snackbar2'));
 $('#leading').on('click',function(event){
 	snackbar2.open();
 });
+
+const snackbar3 = new mdc.snackbar.MDCSnackbar.attachTo(document.querySelector('.mdc-snackbar3'));
 $('#stacked').on('click',function(event){
 	snackbar3.open();
 });
